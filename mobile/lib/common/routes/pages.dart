6970ecc5d311d24e54f10c495e395a95648f3b13
Duy Cuong <leduycuong726@example.com>
2024-07-15 16:44:23 +0700

    set up project structure

---
+++ mobile/lib/common/routes/pages.dart
@@ -0,0 +1,18 @@
+import 'package:get/get.dart';
+
+import 'names.dart';
+
+class AppPages {
+  static const INITIAL = AppRoutes.INITIAL;
+
+  // static final List<GetPage> routes = [
+  //   GetPage(
+  //     name: AppRoutes.INITIAL,
+  //     page: () {},
+  //   ),
+  //   GetPage(
+  //     name: AppRoutes.SIGN_IN,
+  //     page: (){},
+  //   ),
+  // ];
+}
---
+++ mobile/lib/pages/sign_in/index.dart
@@ -0,0 +1,6 @@
+library Sign_in;
+
+export 'controller.dart';
+export 'view.dart';
+export 'binding.dart';
+export 'state.dart';
