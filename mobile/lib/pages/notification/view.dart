ae0acdd6d3fb8776a17ef405a7fbc37cc5df50d8
Duy Cuong <leduycuong726@example.com>
2024-08-09 00:17:36 +0700

    fix lỗi tràn tab

--- mobile/lib/pages/notification/view.dart
+++ mobile/lib/pages/notification/view.dart
@@ -9,180 +9,133 @@ import 'package:health_for_all/pages/notification/widget/notice.dart';
 import 'package:health_for_all/pages/notification/widget/remind.dart';
 import 'package:health_for_all/pages/notification/widget/read.dart';
 
-class NotificationPage extends GetView<NotificationController>{
+class NotificationPage extends GetView<NotificationController> {
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
-      length: 4,
-      child: Scaffold(
-        // thanh chọn
-        appBar: AppBar(
-          bottom: TabBar(
-            tabs: [
+        length: 4,
+        child: Scaffold(
+            // thanh chọn
+            appBar: AppBar(
+                bottom: TabBar(tabs: [
               Tab(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Container(
-                      child: Row(
-                        children: [
-                          Text(
-                              'Chưa đọc',
-                              style: TextStyle(
-                                color: Theme.of(context).colorScheme.onSurfaceVariant,
-                                fontSize: 14,
-                              ),
-                            ),
-                            const SizedBox(width: 5,),
-                            Container(
-                              padding: const EdgeInsets.all(6),
-                              decoration: BoxDecoration(
-                                color: Colors.red[900],
-                                shape: BoxShape.circle,
-                              ),
-                              child: const Center(
-                                child: Text(
-                                  '1',
-                                  style: TextStyle(
-                                    color: Colors.white,
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 11,
-                                  )
-                                ),
-                              )
-                            ),
-                        ],
-                      ),
+                child:
+                    Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                  Text(
+                    'Chưa đọc',
+                    style: TextStyle(
+                      color: Theme.of(context).colorScheme.onSurfaceVariant,
+                      fontSize: 12,
                     ),
-                  ]
-                ),
+                  ),
+                  Container(
+                      padding: const EdgeInsets.all(4),
+                      decoration: BoxDecoration(
+                        color: Colors.red[900],
+                        shape: BoxShape.circle,
+                      ),
+                      child: const Center(
+                        child: Text('1',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 8,
+                            )),
+                      )),
+                ]),
               ),
-
               Tab(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Container(
-                      child: Row(
-                        children: [
-                          Text(
-                              'Cảnh báo',
-                              style: TextStyle(
-                                color: Theme.of(context).colorScheme.onSurfaceVariant,
-                                fontSize: 14,
-                              ),
-                            ),
-                            const SizedBox(width: 5,),
-                            Container(
-                              padding: const EdgeInsets.all(6),
-                              decoration: BoxDecoration(
-                                color: Colors.red[900],
-                                shape: BoxShape.circle,
-                              ),
-                              child: const Center(
-                                child: Text(
-                                  '1',
-                                  style: TextStyle(
-                                    color: Colors.white,
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 11,
-                                  )
-                                ),
-                              )
-                            ),
-                        ],
-                      ),
+                child:
+                    Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                  Text(
+                    'Cảnh báo',
+                    style: TextStyle(
+                      color: Theme.of(context).colorScheme.onSurfaceVariant,
+                      fontSize: 12,
                     ),
-                  ]
-                ),
+                  ),
+                  const SizedBox(
+                    width: 1,
+                  ),
+                  Container(
+                      padding: const EdgeInsets.all(4),
+                      decoration: BoxDecoration(
+                        color: Colors.red[900],
+                        shape: BoxShape.circle,
+                      ),
+                      child: const Center(
+                        child: Text('1',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 8,
+                            )),
+                      )),
+                ]),
               ),
-
               Tab(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Container(
-                      child: Row(
-                        children: [
-                          Text(
-                              'Nhắc nhở',
-                              style: TextStyle(
-                                color: Theme.of(context).colorScheme.onSurfaceVariant,
-                                fontSize: 14,
-                              ),
-                            ),
-                            const SizedBox(width: 5,),
-                            Container(
-                              padding: const EdgeInsets.all(6),
-                              decoration: BoxDecoration(
-                                color: Colors.red[900],
-                                shape: BoxShape.circle,
-                              ),
-                              child: const Center(
-                                child: Text(
-                                  '1',
-                                  style: TextStyle(
-                                    color: Colors.white,
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 11,
-                                  )
-                                ),
-                              )
-                            ),
-                        ],
-                      ),
+                child:
+                    Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                  Text(
+                    'Nhắc nhở',
+                    style: TextStyle(
+                      color: Theme.of(context).colorScheme.onSurfaceVariant,
+                      fontSize: 12,
                     ),
-                  ]
-                ),
+                  ),
+                  const SizedBox(
+                    width: 1,
+                  ),
+                  Container(
+                      padding: const EdgeInsets.all(4),
+                      decoration: BoxDecoration(
+                        color: Colors.red[900],
+                        shape: BoxShape.circle,
+                      ),
+                      child: const Center(
+                        child: Text('1',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 8,
+                            )),
+                      )),
+                ]),
               ),
-
               Tab(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Container(
-                      child: Row(
-                        children: [
-                          Text(
-                              'Đã đọc',
-                              style: TextStyle(
-                                color: Theme.of(context).colorScheme.onSurfaceVariant,
-                                fontSize: 14,
-                              ),
-                            ),
-                            const SizedBox(width: 5,),
-                            Container(
-                              padding: const EdgeInsets.all(6),
-                              decoration: BoxDecoration(
-                                color: Colors.red[900],
-                                shape: BoxShape.circle,
-                              ),
-                              child: const Center(
-                                child: Text(
-                                  '1',
-                                  style: TextStyle(
-                                    color: Colors.white,
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 11,
-                                  )
-                                ),
-                              )
-                            ),
-                        ],
-                      ),
+                child:
+                    Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                  Text(
+                    'Đã đọc',
+                    style: TextStyle(
+                      color: Theme.of(context).colorScheme.onSurfaceVariant,
+                      fontSize: 12,
                     ),
-                  ]
-                ),
+                  ),
+                  const SizedBox(
+                    width: 1,
+                  ),
+                  Container(
+                      padding: const EdgeInsets.all(4),
+                      decoration: BoxDecoration(
+                        color: Colors.red[900],
+                        shape: BoxShape.circle,
+                      ),
+                      child: const Center(
+                        child: Text('1',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 8,
+                            )),
+                      )),
+                ]),
               ),
-            ]
-          )
-        ),
+            ])),
 
-        // liệt kê body
-        body: const TabBarView(
-            children: [Unread(), Notice(), Homepage(), Homepage()],
-          )
-      )
-    );
+            // liệt kê body
+            body: const TabBarView(
+              children: [Unread(), Notice(), Homepage(), Homepage()],
+            )));
   }
 }
