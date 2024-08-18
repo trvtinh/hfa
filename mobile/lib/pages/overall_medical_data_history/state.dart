c3c6f754131b32a0d31dd3c131b45c8e5770d468
Duy Cuong <leduycuong726@gmail.com>
2024-08-19 01:55:10 +0700

    comment and display comment

--- mobile/lib/pages/overall_medical_data_history/state.dart
+++ mobile/lib/pages/overall_medical_data_history/state.dart
@@ -1,8 +1,11 @@
 import 'package:get/get.dart';
+import 'package:health_for_all/common/entities/comment.dart';
 import 'package:health_for_all/common/entities/medical_data.dart';
 
 class OverrallMedicalDataHistoryState {
   RxMap<DateTime, List<MedicalEntity>> history =
       <DateTime, List<MedicalEntity>>{}.obs;
   List<String> dataType = [];
+  RxList<Comment> commmentList = <Comment>[].obs;
+  RxString medicalId = ''.obs;
 }
