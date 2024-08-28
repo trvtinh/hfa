feb23fb3c2986954ac582ae6293b6dd4ff0d162b
TieuDaoChanNhan <khuengv2007@gmail.com>
2024-08-28 22:42:40 +0700

    finish connect_hardware front_end

--- mobile/lib/pages/connect_hardware/widget/find_device.dart
+++ mobile/lib/pages/connect_hardware/widget/find_device.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class FindDevice extends StatefulWidget {
   final bool complete_find_all;
@@ -16,13 +17,19 @@ class _FindDeviceState extends State<FindDevice> {
         mainAxisSize: MainAxisSize.min,
         children: [
           header(),
-          finding(),
+          SizedBox(
+            height: 16,
+          ),
+          if (widget.complete_find_all == false) finding(),
+          list_founded_device(),
+          SizedBox(height: 45,),
+          build_button(),
         ],
       ),
     );
   }
-  
-  Widget header(){
+
+  Widget header() {
     return Row(
       children: [
         Icon(
@@ -30,30 +37,185 @@ class _FindDeviceState extends State<FindDevice> {
           size: 32,
           color: Theme.of(context).colorScheme.primary,
         ),
-        SizedBox(width: 16,),
+        SizedBox(
+          width: 16,
+        ),
         Text(
           "Tìm kiếm thiết bị",
           style: TextStyle(
-            fontSize: 24,
-            color: Theme.of(context).colorScheme.onSurface
-          ),
+              fontSize: 24, color: Theme.of(context).colorScheme.onSurface),
         ),
       ],
     );
   }
 
-  Widget finding(){
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
+  Widget finding() {
+    return Column(
       children: [
-        Image.asset("assets/images/circular-indeterminate.png"),
-        Text(
-          "Đang tìm kiếm",
-          style: TextStyle(
-            color: Theme.of(context).colorScheme.outline,
-            fontSize: 16,
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset("assets/images/circular-indeterminate.png"),
+            SizedBox(
+              width: 16,
+            ),
+            Text(
+              "Đang tìm kiếm",
+              style: TextStyle(
+                color: Theme.of(context).colorScheme.outline,
+                fontSize: 16,
+              ),
+            )
+          ],
+        ),
+        SizedBox(
+          height: 16,
+        ),
+      ],
+    );
+  }
+
+  int number_device = 4;
+  List<String> name_device = [
+    "HFA-Portcare 01",
+    "HFA-Portcare 01",
+    "HFA-Portcare 01",
+    "HFA-Portcare 01",
+  ];
+  List<String> id_device = [
+    "9c3682a0-f27b-490d-b861-5e056d9a1234",
+    "9c3682a0-f27b-490d-b861-5e056d9a1234",
+    "9c3682a0-f27b-490d-b861-5e056d9a1234",
+    "9c3682a0-f27b-490d-b861-5e056d9a1234",
+  ];
+  List<double> distance = [
+    -60,
+    -60,
+    -60,
+    -60,
+  ];
+  Widget list_founded_device() {
+    return Container(
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(4),
+            topRight: Radius.circular(4),
           ),
-        )
+          border: Border.all(color: Theme.of(context).colorScheme.outline)),
+      width: MediaQuery.of(context).size.width - 75,
+      child: Column(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(4),
+                topRight: Radius.circular(4),
+              ),
+              color: Theme.of(context).colorScheme.surfaceContainer,
+            ),
+            padding: EdgeInsets.all(8),
+            child: Row(
+              children: [
+                Text(
+                  "Kết quả tìm kiếm",
+                  style: TextStyle(
+                    color: Theme.of(context).colorScheme.onSurface,
+                    fontSize: 14,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          for (int i = 0; i < number_device; i++)
+            founded_device(name_device[i], id_device[i], distance[i]),
+        ],
+      ),
+    );
+  }
+
+  Widget founded_device(String name_device, String id_device, double distance) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 8),
+      child: Column(
+        children: [
+          SizedBox(
+            height: 16,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        name_device,
+                        style: TextStyle(
+                          color: Theme.of(context).colorScheme.onSurface,
+                          fontSize: 14,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 4,
+                      ),
+                      Text(
+                        id_device,
+                        style: TextStyle(
+                          fontSize: 12,
+                          color: Theme.of(context).colorScheme.onSurfaceVariant,
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+              Row(
+                children: [
+                  Text(
+                    distance.toString(),
+                    style: TextStyle(
+                        fontSize: 12,
+                        color: Theme.of(context).colorScheme.onSurface),
+                  )
+                ],
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget build_button() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+        TextButton(
+          onPressed: () async{
+            Get.back();
+          },
+          child: Text(
+            'Đóng',
+            style: TextStyle(
+              fontSize: 14,
+              color: Theme.of(context).colorScheme.primary,
+            ),
+          ),
+        ),
+        const SizedBox(width: 16),
+        TextButton(
+          onPressed: () async{
+            Get.back();
+          },
+          child: Text(
+            'Xác nhận',
+            style: TextStyle(
+              fontSize: 14,
+              color: Theme.of(context).colorScheme.primary,
+            ),
+          ),
+        ),
       ],
     );
   }
