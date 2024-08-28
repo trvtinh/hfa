c02ae7b701312f34124cf2b962f7da1c07995694
TieuDaoChanNhan <khuengv2007@gmail.com>
2024-08-29 00:25:36 +0700

    pull code về và sửa lại out màn hình add đơn thuốc

--- mobile/lib/pages/alarm/view.dart
+++ mobile/lib/pages/alarm/view.dart
@@ -8,13 +8,44 @@ class AlarmPage extends GetView<AlarmController> {
     return Scaffold(
       appBar: AppBar(
         title: Text(
-          'Cài đặt cảnh báo'
+          'Cài đặt cảnh báo',
+          style: TextStyle(
+            color: Theme.of(context).colorScheme.onSurface,
+            fontSize: 22,
+          ),
         ),
+        actions: [
+          Icon(
+            Icons.help_outline,
+            size: 24,
+            color: Theme.of(context).colorScheme.onSurfaceVariant,
+          ),
+          SizedBox(width: 12,),
+        ],
       ),
-      body: Center(
-        child: Column(
-          children: <Widget>[],
-        ),
+      body: Column(
+        children: [
+          Divider(height: 1,),
+          Container(
+            padding: EdgeInsets.all(16),
+            child: Column(
+              children: [
+                add_alarm()
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget add_alarm(){
+    return Container(
+      padding: EdgeInsets.all(12),
+      child: Row(
+        children: [
+
+        ],
       ),
     );
   }
